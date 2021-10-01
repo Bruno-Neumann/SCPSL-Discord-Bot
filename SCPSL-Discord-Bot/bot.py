3fba072184b28796240c0371ae4721414e7fb393
Bruno <77393226+Bruno-LGS@users.noreply.github.com>
2021-10-01 21:03:14 +0200

    Update bot.py

--- SCPSL-Discord-Bot/bot.py
+++ SCPSL-Discord-Bot/bot.py
@@ -1,8 +1,9 @@
 import asyncio
 import discord
-from discord.ext import commands
 import requests
 import json
+from discord.ext import commands
+
 
 bot = commands.Bot(command_prefix="!")
 @bot.event
