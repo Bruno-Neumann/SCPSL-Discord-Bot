316f01008ee221c217b56ba3d5bec070eb6f416e
Bruno <77393226+Bruno-LGS@users.noreply.github.com>
2021-12-26 00:18:53 +0100

    Update bot.py

--- SCPSL-Discord-Bot/bot.py
+++ SCPSL-Discord-Bot/bot.py
@@ -4,13 +4,15 @@ import requests
 import json
 from discord.ext import commands
 
+id  = ""
+api = ""
 
 bot = commands.Bot(command_prefix="!")
 @bot.event
 async def on_ready():
     print("The bot is running.")
     while True:
-        t = requests.get("https://api.scpslgame.com/serverinfo.php?id=<ID>&key=<API>&players=true").text
+        t = requests.get(f"https://api.scpslgame.com/serverinfo.php?id={id}&key={api}&players=true").text
         data = json.loads(t)
         await bot.change_presence(activity=discord.Game(name=data["Servers"][0]["Players"]))
         print(data["Servers"][0]["Players"])        
