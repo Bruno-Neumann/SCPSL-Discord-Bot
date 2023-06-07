78c99675a3772ccc5dca291cc04a0f305f3af16b
Bruno <77393226+Bruno-LGS@users.noreply.github.com>
2023-06-07 08:53:00 +0200

    Update bot.py

--- SCPSL-Discord-Bot/bot.py
+++ SCPSL-Discord-Bot/bot.py
@@ -3,8 +3,15 @@ import discord
 import requests
 import json
 
+#Insert the api key and Account ID of your server here.
+
 id  = ""
 api = ""
+
+#If you have more than 1 server and want this bot to display the other servers change this value. 
+#1st server = 0, 2nd = 1, 3rd = 2 etc
+
+server = 0
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.none())
 
@@ -14,8 +21,9 @@ async def on_ready():
     while True:
         t = requests.get(f"https://api.scpslgame.com/serverinfo.php?id={id}&key={api}&players=true").text
         data = json.loads(t)
-        await bot.change_presence(activity=discord.Game(name=data["Servers"][0]["Players"]))
-        print(data["Servers"][0]["Players"])        
+        playercount= data["Servers"][server]["Players"]
+        await bot.change_presence(activity=discord.Game(name=playercount))
+        print(playercount)        
         await asyncio.sleep(60)
 
 
