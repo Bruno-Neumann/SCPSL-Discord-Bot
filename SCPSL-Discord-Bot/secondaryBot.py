c2ae662dc662276c6f972cbe3ce69273c35a3c75
Bruno <77393226+Bruno-Neumann@users.noreply.github.com>
2024-07-06 11:44:24 +0200

    Update secondaryBot.py

--- SCPSL-Discord-Bot/secondaryBot.py
+++ SCPSL-Discord-Bot/secondaryBot.py
@@ -6,11 +6,13 @@ import discord
 from discord.ext import commands
 import json
 
+#Set this to False if you dont want the bot to change status depending on player count
 enableStatus = True
+#How often the bot should query the API in seconds
+waitTime = 15
+
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.none())
-
-waitTime = 15
 
 @bot.event
 async def on_ready():
@@ -23,18 +25,17 @@ async def on_ready():
 
             #Change the 0 if you want to display another server
             #0 = 1st server, 1 = 2nd server, 2 = 3rd server and so on.
-            playercount= data["Servers"][1]["Players"]
+            playercount = data["Servers"][1]["Players"]
 
             if enableStatus == True:
-                sep = '/'
-                totalPlayers = int(playercount.split(sep, 1)[0])
-                totalSlots = int(playercount.split(sep, 1)[1])
+                totalPlayers = int(playercount.split("/", 1)[0])
+                totalSlots = int(playercount.split("/", 1)[1])
                 if totalPlayers == 0:
                     stat = discord.Status.idle
                 elif totalPlayers == totalSlots:
-                    stat= discord.Status.dnd
+                    stat = discord.Status.dnd
                 else:
-                    stat= discord.Status.online
+                    stat = discord.Status.online
 
             await bot.change_presence(status=stat,activity=discord.Game(name=playercount))
             print(playercount)     
